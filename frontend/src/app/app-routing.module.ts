e90db5ae03121c95b6fb892ba2487eb6c1a45387
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-03-09 18:17:48 -0500

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCarrerasComponent } from './components/carreras/add-carreras/add-carreras.component';
+import { EditCarrerasComponent } from './components/carreras/edit-carreras/edit-carreras.component';
+import { ViewCarrerasComponent } from './components/carreras/view-carreras/view-carreras.component';
+import { AddEstudiantesComponent } from './components/estudiantes/add-estudiantes/add-estudiantes.component';
+import { EditEstudiantesComponent } from './components/estudiantes/edit-estudiantes/edit-estudiantes.component';
+import { ViewEstudiantesComponent } from './components/estudiantes/view-estudiantes/view-estudiantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/estudiantes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'estudiantes',
+    component: ViewEstudiantesComponent
+  },
+  {
+    path: 'carreras',
+    component: ViewCarrerasComponent
+  },
+  {
+    path: 'estudiantes/add',
+    component: AddEstudiantesComponent
+  },
+  {
+    path: 'estudiantes/edit/:id',
+    component: EditEstudiantesComponent
+  },
+  {
+    path: 'carreras/add',
+    component: AddCarrerasComponent
+  },
+  {
+    path: 'carreras/edit/:id',
+    component: EditCarrerasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
